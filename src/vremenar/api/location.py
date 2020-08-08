5b538b8b033e514b433059d39bf20a51952157b3
Tadej Novak <tadej@tano.si>
2020-08-08 15:29:42 +0200

    Properly handle POST body

--- src/vremenar/api/location.py
+++ src/vremenar/api/location.py
@@ -1,6 +1,6 @@
 """Weather conditions map API."""
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Body
 from typing import List, Optional
 
 from ..definitions import CountryID
@@ -8,14 +8,15 @@ from ..models.weather import WeatherInfo
 from ..sources import find_location
 
 router = APIRouter()
+_empty_body = Body(None)
 
 
 @router.post('/location/find', tags=['location'])
 async def find(
     country: CountryID,
-    string: Optional[str] = None,
-    latitude: Optional[float] = None,
-    longitude: Optional[float] = None,
+    string: Optional[str] = _empty_body,
+    latitude: Optional[float] = _empty_body,
+    longitude: Optional[float] = _empty_body,
 ) -> List[WeatherInfo]:
     """Get weather conditions map for a specific ID."""
     return await find_location(country, string, latitude, longitude)
