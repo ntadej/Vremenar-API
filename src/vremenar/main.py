50bcdea8b9bb9c67e25ab2e5769e41d3d6a532ef
Tadej Novak <tadej@tano.si>
2020-11-01 19:14:35 +0100

    Add gzip middleware

--- src/vremenar/main.py
+++ src/vremenar/main.py
@@ -1,6 +1,7 @@
 """Vremenar backend main application."""
 
 from fastapi import FastAPI
+from fastapi.middleware.gzip import GZipMiddleware
 
 from .api import version
 from .api import location
@@ -8,6 +9,7 @@ from .api import maps
 from .api import weather_map
 
 app = FastAPI()
+app.add_middleware(GZipMiddleware)
 app.include_router(version)
 app.include_router(location)
 app.include_router(maps)
