7e3684678c60c6f7ce3fce3d4aed5bfea7ade926
Tadej Novak <tadej@tano.si>
2020-11-07 22:47:42 +0100

    Cleanup version API

--- src/vremenar/api/version.py
+++ src/vremenar/api/version.py
@@ -1,20 +1,29 @@
 """App version API."""
 
 from fastapi import APIRouter
+from pydantic import BaseModel, Field
 
 router = APIRouter()
 
 
-class VersionInfo:
+class VersionInfo(BaseModel):
     """Version info."""
 
-    def __init__(self) -> None:
-        """Initialize version info."""
-        self.stable: str = ''
-        self.beta: str = '0.2.0'
+    stable: str = Field('', const=True, example='1.0.0')
+    beta: str = Field('0.2.0', const=True, example='0.2.0')
+
+    class Config:
+        """Version info config."""
+
+        title: str = 'Version info'
 
 
-@router.get('/version', tags=['version'])
+@router.get(
+    '/version',
+    tags=['version'],
+    response_description='Get Vremenar versions',
+    response_model=VersionInfo,
+)
 async def version() -> VersionInfo:
     """Get app version."""
     return VersionInfo()
