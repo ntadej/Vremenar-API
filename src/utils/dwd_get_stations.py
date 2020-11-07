c107e146cfcf96e1d9646932f67e004ceef499c7
Tadej Novak <tadej@tano.si>
2020-11-07 19:39:17 +0100

    Utilities: dump DWD stations summary

--- src/utils/dwd_get_stations.py
+++ src/utils/dwd_get_stations.py
@@ -21,10 +21,17 @@ with open(file_name, 'r') as file:
     data = load(file)
 
 stations = {}
+dwd_map = {}
+wmo_map = {}
+dwd_set = set()
+wmo_set = set()
 for r in data:
     dwd_station_id = r['dwd_station_id']
     wmo_station_id = r['wmo_station_id']
-    station_id = dwd_station_id if dwd_station_id else wmo_station_id
+    station_id = wmo_station_id
+
+    dwd_set.add(dwd_station_id)
+    wmo_set.add(wmo_station_id)
 
     name = r['station_name']
     if name == 'SWIS-PUNKT':
@@ -33,9 +40,28 @@ for r in data:
     lat = r['lat']
     lon = r['lon']
 
-    stations[station_id] = {'name': name, 'lat': lat, 'lon': lon}
+    stations[station_id] = {
+        'name': name,
+        'lat': lat,
+        'lon': lon,
+        'DWD': dwd_station_id,
+        'WMO': wmo_station_id,
+    }
+
+    wmo_map[wmo_station_id] = dwd_station_id
+    if dwd_station_id:
+        dwd_map[dwd_station_id] = wmo_station_id
+
+print(len(stations.keys()))
+
+overlap = list(dwd_set.intersection(wmo_set))
 
 file_out = DWD_CACHE_DIR / 'stations_list.json'
 print(file_out)
 with open(file_out, 'w') as file:
-    dump(stations, file)
+    dump(stations, file, indent=4)
+
+file_out = DWD_CACHE_DIR / 'stations_ID_map.json'
+print(file_out)
+with open(file_out, 'w') as file:
+    dump({'DWD': dwd_map, 'WMO': wmo_map, 'overlap': overlap}, file, indent=4)
