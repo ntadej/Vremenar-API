3aa2e8b8dbf3e9b444b436cbc13d7fe05c2fe09c
Tadej Novak <tadej@tano.si>
2020-11-07 23:14:40 +0100

    Add metadata

--- src/vremenar/main.py
+++ src/vremenar/main.py
@@ -7,7 +7,27 @@ from .api import version
 from .api import stations
 from .api import maps
 
-app = FastAPI()
+tags_metadata = [
+    {
+        'name': 'version',
+        'description': 'Vremenar version infromation used for update notifications.',
+    },
+    {
+        'name': 'stations',
+        'description': 'Weather stations information and maps,'
+        'including weather information.',
+    },
+    {
+        'name': 'maps',
+        'description': 'Query available weather maps and their information.',
+    },
+]
+
+app = FastAPI(
+    title='Vremenar API',
+    description='Weather API powering Vremenar application',
+    openapi_tags=tags_metadata,
+)
 app.add_middleware(GZipMiddleware)
 app.include_router(version)
 app.include_router(stations)
