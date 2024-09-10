dd53b0d556e42b2cbbb0f3a23edc7fc14cc3326f
Tadej Novak <tadej@tano.si>
2024-09-10 20:11:37 +0200

    Update dependencies

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,19 @@
 """Tests configuration."""
 
-import asyncio
-from collections.abc import AsyncGenerator, Generator
+from collections.abc import AsyncGenerator
 from typing import Any
 
 import pytest
 from httpx import ASGITransport, AsyncClient
-
-# def pytest_collection_modifyitems(items: list[Any]) -> None:
-#     """Add session scope to async tests."""
-#     pytest_asyncio_tests = (item for item in items if is_async_test(item))
-#     session_scope_marker = pytest.mark.asyncio(scope="session")
-#     for async_test in pytest_asyncio_tests:
-#         async_test.add_marker(session_scope_marker)
+from pytest_asyncio import is_async_test
 
 
-@pytest.fixture(scope="session")
-def event_loop(
-    request: Any,  # noqa: ANN401, ARG001
-) -> Generator[asyncio.AbstractEventLoop, None, None]:
-    """Create an instance of the default event loop for each test case."""
-    loop = asyncio.get_event_loop_policy().new_event_loop()
-    yield loop
-    loop.close()
+def pytest_collection_modifyitems(items: list[Any]) -> None:
+    """Add session scope to async tests."""
+    pytest_asyncio_tests = (item for item in items if is_async_test(item))
+    session_scope_marker = pytest.mark.asyncio(loop_scope="session")
+    for async_test in pytest_asyncio_tests:
+        async_test.add_marker(session_scope_marker, append=False)
 
 
 @pytest.fixture(scope="module")
